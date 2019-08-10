b363b202f087d570cd0d32cc864daad04de1f1ac
Dmitry Plevkov <wwarne@gmail.com>
2019-08-10 18:35:43 +0300

    rewrite + refactor the service from my other repository.

--- server.py
+++ server.py
@@ -2,6 +2,26 @@ import argparse
 
 from aiohttp import web
 import aiofiles
+import os
+import logging
+import pathlib
+
+
+class ArchiveDownloadService:
+    def __init__(self, base_directory: str, download_delay: float, logging_enabled: bool):
+        self.base_directory = pathlib.Path(base_directory)
+        self.download_delay = download_delay
+        self.logger = logging.getLogger('archive_service')
+        self.logger.level = logging.DEBUG if logging_enabled else logging.NOTSET
+
+    async def handle_index_page(self, request: web.Request) -> web.Response:
+        async with aiofiles.open('index.html', mode='r') as index_file:
+            index_content = await index_file.read()
+        return web.Response(text=index_content, content_type='text/html')
+
+    async def archivate(self, request: web.Request) -> web.Response:
+        pass
+
 
 
 async def archivate(request):
@@ -26,9 +46,22 @@ def create_parser():
 
 
 if __name__ == '__main__':
+    parser = create_parser()
+    options = parser.parse_args()
+    logging.basicConfig(format='%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s')
+
+    base_directory = options.folder or os.getenv('DVMN_FOLDER', './test_photos/')
+    download_delay = options.delay if options.delay is not None else float(os.getenv('DVMN_DELAY', '0'))
+    logging_enabled = options.logs or bool(os.getenv('DVMN_LOGS', False))
+
+    download_service = ArchiveDownloadService(
+        base_directory=base_directory,
+        download_delay=download_delay,
+        logging_enabled=logging_enabled,
+    )
     app = web.Application()
     app.add_routes([
-        web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archivate),
+        web.get('/', download_service.handle_index_page),
+        web.get('/archive/{archive_hash}/',  download_service.archivate),
     ])
     web.run_app(app)
